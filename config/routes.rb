023b5325596684e3467e6e469e94364fc07052cf
bbjarvs <brettbjarvis@gmail.com>
2022-01-18 06:03:52 +0000

    add auth pages

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,13 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # namespace :admin do
   #   root to: 'dashboard#show'
